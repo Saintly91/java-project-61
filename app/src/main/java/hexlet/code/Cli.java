57fc3b16992d6b876b498f0079ca574a857644bf
Saintly <individ91@mail.ru>
2025-03-23 14:20:31 +0200

    add Cli.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greetUser() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name);
+
+        sc.close();
+    }
+}
